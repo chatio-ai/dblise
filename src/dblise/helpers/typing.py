15c50a510f0d53e10b18d5aa1f0c96562938421a
Roman Valov <roman.valov@gmail.com>
2026-08-28 05:42:10 +0000

    ref: convert find type function into type id parse

--- src/dblise/helpers/typing.py
+++ src/dblise/helpers/typing.py
@@ -3,6 +3,8 @@ from dataclasses import dataclass
 from dataclasses import fields
 from dataclasses import Field
 
+from typing import Self
+
 from types import UnionType
 from types import NoneType
 
@@ -12,27 +14,27 @@ class TypeId:
     raw_type: type
     optional: bool
 
+    @classmethod
+    def parse(cls, type_def: type | UnionType) -> Self:
 
-def find_type(type_def: type | UnionType) -> TypeId:
+        if not isinstance(type_def, UnionType):
+            return cls(
+                raw_type=type_def,
+                optional=False,
+            )
 
-    if not isinstance(type_def, UnionType):
-        return TypeId(
-            raw_type=type_def,
-            optional=False,
-        )
+        if type_def.__args__[1:] == (NoneType,):
+            return cls(
+                raw_type=type_def.__args__[0],
+                optional=True,
+            )
 
-    if type_def.__args__[1:] == (NoneType,):
-        return TypeId(
-            raw_type=type_def.__args__[0],
-            optional=True,
-        )
-
-    raise TypeError
+        raise TypeError
 
 
 def type_ids(cls: type) -> dict[str, TypeId]:
     def _type_id[T](field: Field[T]) -> TypeId:
         assert isinstance(field.type, type | UnionType)
-        return find_type(field.type)
+        return TypeId.parse(field.type)
 
     return {field.name: _type_id(field) for field in fields(cls)}
