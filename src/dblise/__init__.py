341a902c3a095daa6e7c6966e4267f96fbed01fa
Roman Valov <roman.valov@gmail.com>
2026-08-29 02:38:43 +0000

    add: dynamic single entity composition using casts

--- src/dblise/__init__.py
+++ src/dblise/__init__.py
@@ -1,6 +1,8 @@
 
 from abc import ABC, abstractmethod
 
+from typing import cast
+
 from .helpers import typing
 
 from .schemas import Fields
@@ -39,25 +41,31 @@ class Facade(ABC):
     def handle(self, parent: str, child: str) -> str:
         ...
 
+    def _entity(self, handle: str, type_id: typing.KeyTypeId) -> Entity:
+        match type_id:
+            case _ if type_id.entity is Domain:
+                return self.domain(handle)
+            case _ if type_id.entity is Record:
+                assert type_id.fields is not None
+                return self.record(handle, type_id.fields)
+            case _ if type_id.entity is Lookup:
+                assert type_id.fields is not None
+                return self.lookup(handle, type_id.fields)
+            case _ if type_id.entity is Scores:
+                return self.scores(handle)
+            case _ if type_id.entity is Stream:
+                assert type_id.fields is not None
+                return self.stream(handle, type_id.fields)
+            case _:
+                raise TypeError(type_id)
+
+    def entity[EntityT: Entity](self, handle: str, entity: type[EntityT]) -> EntityT:
+        type_id = typing.KeyTypeId.parse(entity)
+        return cast(EntityT, self._entity(handle, type_id))
+
     def schema[SchemaT: Schema](self, handle: str, schema: type[SchemaT]) -> SchemaT:
         result: dict[str, Entity] = {}
         for name, type_id in typing.key_type_ids(schema).items():
-            child = self.handle(handle, name)
-            match type_id.entity:
-                case _ if type_id.entity is Domain:
-                    result[name] = self.domain(child)
-                case _ if type_id.entity is Record:
-                    assert type_id.fields is not None
-                    result[name] = self.record(child, type_id.fields)
-                case _ if type_id.entity is Lookup:
-                    assert type_id.fields is not None
-                    result[name] = self.lookup(child, type_id.fields)
-                case _ if type_id.entity is Scores:
-                    result[name] = self.scores(child)
-                case _ if type_id.entity is Stream:
-                    assert type_id.fields is not None
-                    result[name] = self.stream(child, type_id.fields)
-                case _:
-                    raise TypeError(type_id)
+            result[name] = self._entity(self.handle(handle, name), type_id)
 
         return schema(**result)
