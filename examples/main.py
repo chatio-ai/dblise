ba41bfff669f96910d4c129d9ee8fab8e7666eed
Roman Valov <roman.valov@gmail.com>
2026-08-29 11:55:29 +0000

    wip: stub implementation of facade pipeline method

---
+++ examples/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+# ruff: noqa: T201
+
+import asyncio
+
+from dataclasses import dataclass
+
+from dblise.schemas import Fields
+from dblise.schemas import Record
+from dblise.schemas import Schema
+
+from dblise import Facade
+from dblise.redisdb import RedisFacade
+
+
+@dataclass
+class Test(Fields):
+    data: str
+
+
+@dataclass(frozen=True)
+class Tests(Schema):
+    test: Record[Test]
+
+
+def _facade() -> Facade:
+    return RedisFacade()
+
+
+async def main() -> None:
+    facade = _facade()
+    schema = facade.schema('test', Tests)
+
+    await schema.test.assign(Test('hello'))
+    print(await schema.test.value())
+
+    async with facade.pipeline():
+        await schema.test.value()
+        await schema.test.assign(Test('world'))
+
+    print(await schema.test.value())
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
