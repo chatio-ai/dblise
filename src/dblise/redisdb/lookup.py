a75462b40c1ef5dc48c75c6a5dfde6e21fa0e5b4
Roman Valov <roman.valov@gmail.com>
2026-08-31 13:24:40 +0000

    ref: untie redis lookup from redis domain entities

--- src/dblise/redisdb/lookup.py
+++ src/dblise/redisdb/lookup.py
@@ -7,16 +7,30 @@ from dblise.schemas import Lookup
 
 from .common import Redis
 from .codecs import RedisCodecs
-from .domain import RedisDomain
+from .entity import RedisEntity
 from .record import RedisRecord
 
 
-class RedisLookup[FieldsT: Fields](RedisDomain, Lookup[FieldsT]):
+class RedisLookup[FieldsT: Fields](RedisEntity, Lookup[FieldsT]):
 
     def __init__(self, redis_db: Redis, key_path: str, converts: RedisCodecs[FieldsT]) -> None:
         super().__init__(redis_db, key_path)
         self._converts = converts
+        self._key_glob = f'{key_path}:*'
 
     @override
     def lookup(self, key: str) -> Record[FieldsT]:
         return RedisRecord(self._redis_db, f'{self._key_path}:{key}', self._converts)
+
+    @override
+    async def exists(self) -> bool:
+        async for _ in self._redis_db.scan_iter(self._key_glob):
+            return True
+        return False
+
+    @override
+    async def delete(self) -> bool:
+        keys = [_ async for _ in self._redis_db.scan_iter(self._key_glob)]
+        if keys:
+            await self._redis_db.unlink(*keys)
+        return bool(keys)
